6ae11246424fec2f77c60624d7cb89c61ff50a62
Scott <scott_boylejs@hotmail.com>
2022-01-13 07:53:27 +0000

    fixed wrong background color on some routes

--- src/components/Global/Footer/Footer.js
+++ src/components/Global/Footer/Footer.js
@@ -3,8 +3,9 @@ import { useLocation } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
+  const location = useLocation().pathname.split("/")[1]
   return (
-    <div  className = "footer" style={{background: useLocation().pathname.split("/")[1] === "movie"  ? "rgba(100,100,105,1)" : "white"}}>
+    <div  className = "footer" style={{background: location === "movie" || location === "tv"  ? "rgba(100,100,105,1)" : "white"}}>
       <a target="_blank" href="https://icons8.com/icon/qdQpy48X3Rjv/star">Star</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
    </div>
   )
--- src/components/Global/Header/Header.js
+++ src/components/Global/Header/Header.js
@@ -3,8 +3,11 @@ import './Header.css';
 import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+
+  //Will return "tv" or movie if we are on a movieDetail route
+  const location = useLocation().pathname.split("/")[1]
   return (
-    <div className = "header" style={{background: useLocation().pathname.split("/")[1] === "movie"  ? "rgba(100,100,105,1)" : "white"}}>
+    <div className = "header" style={{background: location === "movie" || location === "tv"   ? "rgba(100,100,105,1)" : "white"}}>
       <Link to ="/"> home </Link>
       <Link to = "/SeeMore/PopularMovies"> movies </Link>
       <Link to = "/SeeMore/PopularTVSeries"> tv shows </Link>

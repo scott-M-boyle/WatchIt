c35bff5557a63da88e0dab31f39bf3d1b695d7ef
Scott <scott_boylejs@hotmail.com>
2022-01-13 07:59:30 +0000

    Updated overlay

--- src/components/Global/ImageCard/ImageCard.js
+++ src/components/Global/ImageCard/ImageCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import './ImageCard.css';
+
   const ImageCard = (props) =>{
+    console.log(props)
   return <div className = "section-poster" key={props.id}>
   <Link
-    to={`/${props.type}/${props.id}`}
-    state={{mediaType: props.type}}> <img src={`https://image.tmdb.org/t/p/original/${props.poster_path}`} alt={props.title} /> </Link>
+    to={`/${props.type}/${props.id}`}> <img src={`https://image.tmdb.org/t/p/original/${props.poster_path}`} alt={props.title} /> </Link>
       <div className = "poster-hidden">
-        <h3> Like this film? </h3>
-        <button className = "see-similar"> See similar </button>
+        <h3> {props.title} </h3>
+        <div className = "poster-buttons">
+          <button className = "see-similar"> See similar </button>
+          <button className = "see-similar"> See similar </button>
+        </div>
       </div>
   </div>
 }
